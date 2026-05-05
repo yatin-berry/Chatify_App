87029acc4f951f95302539d14c3336f891606b02
Yatin Berry <yatinberry201@gmail.com>
2026-05-05 16:17:31 +0530

    Fix cross-domain authentication: changed cookie sameSite to none

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days to milliseconds
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: ENV.NODE_ENV === "production" ? "none" : "lax",
         secure: ENV.NODE_ENV === "production",
     });
 
